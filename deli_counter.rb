9f9abdba26cc48382720b29e7b716457fc6f1f71
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-11 16:04:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,26 @@
 
 def line(customers)
   
-  puts "The line is currently empty." if customers.empty?
+  if customers.empty?
+    puts "The line is currently empty." 
+    
+  else
   
+    puts "The line is currently: "
+    
+    customers.each_with_index do |person, position|
+      print "#" 
+    end
+  
+  end
   
 end
 
 
-def take_a_number(peopel_in_line, )
+def take_a_number(people_in_line, person_joining)
+  
+  people_in_line << person_joining
+  
+  puts ""
+  
+end
